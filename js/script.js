da4c31515f5a0b9d2b19133626ba5e717cd32e56
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-09-03 15:44:04 -0300

    daedead

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let one = document.querySelector('#one')
+let two = document.querySelector('#two')
+let three = document.querySelector('#three')
+let four = document.querySelector('#four')
+let five = document.querySelector('#five')
+let six = document.querySelector('#six')
+let seven = document.querySelector('#seven')
+let eight = document.querySelector('#eight')
+let nine = document.querySelector('#nine')
+
+
+one.addEventListener('click')
+
+
+function addNumber(){
+
+}
